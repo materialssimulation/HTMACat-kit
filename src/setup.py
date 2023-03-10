863affec8f5d69e9ecfc0c8d7d22f46fd06cf767
wuyaos <wufeifeng_hust@163.com>
2023-03-10 09:19:40 +0800

    修改python需要的依赖及其版本

--- src/setup.py
+++ src/setup.py
@@ -10,8 +10,10 @@ with open('requirements.txt', 'r') as f:
 setup(
     name='HTMACat',
     version='1.0.0',
-    description='A high-throughput modeling, calculation, and analysis framework for catalytic reaction processes.',
-    long_description='This kit it provides key tools for high-throughput design and screening of catalytic materials. The software mainly includes functional modules such as surface structure analysis and information extraction, catalytic surface and various adsorption model construction, automatic construction of primitive reaction processes, automatic extraction of computational data and automatic extraction and construction of descriptors.',
+    description=
+    'A high-throughput modeling, calculation, and analysis framework for catalytic reaction processes.',
+    long_description=
+    'This kit it provides key tools for high-throughput design and screening of catalytic materials. The software mainly includes functional modules such as surface structure analysis and information extraction, catalytic surface and various adsorption model construction, automatic construction of primitive reaction processes, automatic extraction of computational data and automatic extraction and construction of descriptors.',
     author='Jiaqiang Yang, Feifeng Wu, Bin Shan',
     author_email='bshan@mail.hust.edu.cn',
     url='http://www.materialssimulation.com/',
@@ -20,10 +22,8 @@ setup(
     install_requires=requirements,
     packages=['HTMACat', "HTMACat.descriptor", "HTMACat.model", "HTMACat.NEB"],
     entry_points={
-        'console_scripts': [ # 命令的入口
+        'console_scripts': [  # 命令的入口
             'ads=HTMACat.command:ads',
             'coads=HTMACat.command:coads',
         ]
-    }
-
-)
+    })
