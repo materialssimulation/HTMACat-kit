480b9f8782283665cbb2799fb607ad08bc4708b6
hjli <1197946404@qq.com>
2023-03-24 10:50:38 +0800

    update test

--- tests/test_ads.py
+++ tests/test_ads.py
@@ -1,7 +1,8 @@
 import pytest
 import os
 import filecmp
-
+from ase.io.vasp import read_vasp
+from ase import Atoms
 @pytest.mark.parametrize("dir1, dir2", [
     ("./example/Formic_acid_ads", "./tests/results/Formic_acid_ads"),
     ("./example/Formic_acid_ads-SML", "./tests/results/Formic_acid_ads-SML")
@@ -25,6 +26,41 @@ def compare_folders(folder1, folder2):
         print(folder1+"缺少文件")
         return False
 
+    for file in files1:
+        if file == 'Model' or file == 'StrucInfo':
+            continue
+        file1_path = os.path.join(folder1, file)
+        file2_path = os.path.join(folder2, file)
+
+        # 读取两个VASP文件
+        atoms1 = read_vasp(file1_path)
+        atoms2 = read_vasp(file2_path)
+
+        # 将VASP结构对象转换为ASE结构对象
+        atoms1 = Atoms(cell=atoms1.get_cell(),
+                    positions=atoms1.get_positions(),
+                    symbols=atoms1.get_chemical_symbols(),
+                    pbc=True)
+        atoms2 = Atoms(cell=atoms2.get_cell(),
+                    positions=atoms2.get_positions(),
+                    symbols=atoms2.get_chemical_symbols(),
+                    pbc=True)
+
+        # 判断两个ASE结构对象是否一样
+        if atoms1 == atoms2:
+            # print('The two structures are identical.')
+            continue
+        else:
+            with open(file1_path,'r') as f:
+                print('file1')
+                print(f.read())
+            with open(file2_path,'r') as f:
+                print('file2')
+                print(f.read())
+            print(f"文件 {file} 不同")
+            # print('The two structures are different.')
+            return False
+    return True
     # 逐个比较文件内容
     # for file in files1:
     #     file1_path = os.path.join(folder1, file)
--- tests/test_coads.py
+++ tests/test_coads.py
@@ -2,7 +2,8 @@ import pytest
 import os
 import filecmp
 import hashlib
-
+from ase.io.vasp import read_vasp
+from ase import Atoms
 @pytest.mark.parametrize("dir1, dir2", [
     ("./example/coads", "./tests/results/coads"),
     ("./example/coads-SML", "./tests/results/coads-SML")
@@ -28,8 +29,41 @@ def compare_folders(folder1, folder2):
 
     # 逐个比较文件内容
     for file in files1:
+        if file == 'Model' or file == 'StrucInfo':
+            continue
         file1_path = os.path.join(folder1, file)
         file2_path = os.path.join(folder2, file)
+
+        # 读取两个VASP文件
+        atoms1 = read_vasp(file1_path)
+        atoms2 = read_vasp(file2_path)
+
+        # 将VASP结构对象转换为ASE结构对象
+        atoms1 = Atoms(cell=atoms1.get_cell(),
+                    positions=atoms1.get_positions(),
+                    symbols=atoms1.get_chemical_symbols(),
+                    pbc=True)
+        atoms2 = Atoms(cell=atoms2.get_cell(),
+                    positions=atoms2.get_positions(),
+                    symbols=atoms2.get_chemical_symbols(),
+                    pbc=True)
+
+        # 判断两个ASE结构对象是否一样
+        if atoms1 == atoms2:
+            # print('The two structures are identical.')
+            continue
+        else:
+            with open(file1_path,'r') as f:
+                print('file1')
+                print(f.read())
+            with open(file2_path,'r') as f:
+                print('file2')
+                print(f.read())
+            print(f"文件 {file} 不同")
+            # print('The two structures are different.')
+            return False
+    return True
+
         # if not filecmp.cmp(file1_path, file2_path, shallow=False):
         #     with open(file1_path,'r') as f:
         #         print('file1')
@@ -40,24 +74,25 @@ def compare_folders(folder1, folder2):
         #     print(f"文件 {file} 不同")
         #     return False
 
-        file1_md5 = calculate_md5(file1_path)
-        file2_md5 = calculate_md5(file2_path)
-        if file1_md5 != file2_md5:
-            with open(file1_path,'r') as f:
-                print('file1')
-                print(f.read())
-            with open(file2_path,'r') as f:
-                print('file2')
-                print(f.read())
-            print(f"文件 {file} 不同")
-            return False
+        # file1_md5 = calculate_md5(file1_path)
+        # file2_md5 = calculate_md5(file2_path)
+        # if file1_md5 != file2_md5:
+        #     with open(file1_path,'r') as f:
+        #         print('file1')
+        #         print(f.read())
+        #     with open(file2_path,'r') as f:
+        #         print('file2')
+        #         print(f.read())
+        #     print(f"文件 {file} 不同")
+        #     return False
 
 
     # 文件夹中的文件全部相同
-    return True
+    # return True
 
-def calculate_md5(file_path):
-    with open(file_path, 'rb') as f:
-        content = f.read()
-        md5_hash = hashlib.md5(content)
-        return md5_hash.hexdigest()
+# def calculate_md5(file_path):
+#     with open(file_path, 'rb') as f:
+#         content = f.read()
+#         content = content.strip()
+#         md5_hash = hashlib.md5(content)
+#         return md5_hash.hexdigest()
