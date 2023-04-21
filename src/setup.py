fd5f91899b3bda9227d2c172afa0a5931ebc1aa6
wuyaos <wufeifeng_hust@163.com>
2023-04-22 02:38:21 +0800

    更新打包文件列表

--- src/setup.py
+++ src/setup.py
@@ -7,11 +7,11 @@ setup(
     name='HTMACat',
     version='1.0.4',
     classifiers=[
-    'Development Status :: 3 - Alpha',
-    'Environment :: Console',
-    'License :: OSI Approved :: GUN General Public License v3 (GPLv3)',
-    'Operating System :: OS Independent',
+        'Development Status :: 3 - Alpha', 'Environment :: Console',
+        'License :: OSI Approved :: GUN General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent'
     ],
+    license='GPLv3',
     description=
     'A high-throughput modeling, calculation, and analysis framework for catalytic reaction processes.',
     long_description=
@@ -19,10 +19,12 @@ setup(
     author='Jiaqiang Yang, Feifeng Wu, Bin Shan',
     author_email='bshan@mail.hust.edu.cn',
     url='https://stanfordbshan.github.io/HTMACat-kit/',
-    keywords=['high-throughput','python', 'catalysis', 'modeling'],
+    keywords=['high-throughput', 'python', 'catalysis', 'modeling'],
     python_requires=">=3.6, <3.10",
     install_requires=requirements,
     packages=find_packages(),
+    include_package_data=True,
+    package_data={'': ["*.db", "*.txt", "*.json", "*.yaml", "*.md", "*.rst", "Element_Info"]},
     entry_points={
         'console_scripts': [  # 命令的入口
             'htmat=HTMACat.command:main',
