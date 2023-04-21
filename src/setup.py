db259e04efea2e0daf8f680b0d5d727b482bc927
wuyaos <wufeifeng_hust@163.com>
2023-04-22 02:14:29 +0800

    更新打包文件

--- src/setup.py
+++ src/setup.py
@@ -6,14 +6,20 @@ with open('requirements.txt', 'r') as f:
 setup(
     name='HTMACat',
     version='1.0.4',
+    classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Environment :: Console',
+    'License :: OSI Approved :: GUN General Public License v3 (GPLv3)',
+    'Operating System :: OS Independent',
+    ],
     description=
     'A high-throughput modeling, calculation, and analysis framework for catalytic reaction processes.',
     long_description=
     'This kit it provides key tools for high-throughput design and screening of catalytic materials. The software mainly includes functional modules such as surface structure analysis and information extraction, catalytic surface and various adsorption model construction, automatic construction of primitive reaction processes, automatic extraction of computational data and automatic extraction and construction of descriptors.',
     author='Jiaqiang Yang, Feifeng Wu, Bin Shan',
     author_email='bshan@mail.hust.edu.cn',
-    url='http://www.materialssimulation.com/',
-    keywords='high-throughput',
+    url='https://stanfordbshan.github.io/HTMACat-kit/',
+    keywords=['high-throughput','python', 'catalysis', 'modeling'],
     python_requires=">=3.6, <3.10",
     install_requires=requirements,
     packages=find_packages(),
