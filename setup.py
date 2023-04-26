bed459f14b6f6d5643a46933f2adf8338ce37f06
wuyaos <wuyaos@users.noreply.github.com>
2023-04-26 18:31:46 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,20 +3,31 @@ from setuptools import setup, find_packages
 with open('requirements.txt', 'r') as f:
     requirements = f.readlines()
 
+with open('README.md', 'r', encoding='utf-8') as f:
+    readme = f.read()
+
 setup(
     name='HTMACat',
     version='1.0.5',
+    classifiers=[
+        'Development Status :: 3 - Alpha', 'Environment :: Console',
+        'Operating System :: OS Independent'
+    ],
+    license='GPLv3',
     description=
     'A high-throughput modeling, calculation, and analysis framework for catalytic reaction processes.',
-    long_description=
-    'This kit it provides key tools for high-throughput design and screening of catalytic materials. The software mainly includes functional modules such as surface structure analysis and information extraction, catalytic surface and various adsorption model construction, automatic construction of primitive reaction processes, automatic extraction of computational data and automatic extraction and construction of descriptors.',
+    long_description_content_type="text/markdown",
+    long_description=readme,
     author='Jiaqiang Yang, Feifeng Wu, Bin Shan',
     author_email='bshan@mail.hust.edu.cn',
-    url='http://www.materialssimulation.com/',
-    keywords='high-throughput',
+    url='https://stanfordbshan.github.io/HTMACat-kit/',
+    keywords=['high-throughput', 'python', 'catalysis', 'modeling'],
     python_requires=">=3.6, <3.10",
     install_requires=requirements,
     packages=find_packages(),
+    include_package_data=True,
+    readme='README.md',
+    package_data={'': ["*.db", "*.txt", "*.json", "*.yaml", "*.md", "*.rst", "Element_Info"]},
     entry_points={
         'console_scripts': [  # 命令的入口
             'htmat=HTMACat.command:main',
