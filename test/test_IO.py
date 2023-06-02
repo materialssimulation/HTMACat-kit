5c5689945d5af8646f243c7f5b897859a91a28bd
hust-rxchen <rongxinchen@hust.edu.cn>
2023-06-02 10:21:21 +0800

    修复了有dope时修改slab层数报错的bug，修复建立supercell不能实际生效的bug

--- test/test_IO.py
+++ test/test_IO.py
@@ -23,8 +23,8 @@ def result():
         "StrucInfo": {
             "struct": {
                 "element": "Pt",
-                "lattype": "fcc",
-                "latcont": 3.92,
+                "lattice_type": "fcc",
+                "lattice_constant": 3.92,
                 "facet": ["111"],
                 "dope": {"Cu": [0]},
             }
@@ -41,7 +41,7 @@ def result():
 def test_substrate_part(result):
     substrates = substrate_part(result)
     assert substrates[0].facet == "111"
-    assert substrates[0].bulk.lattice_constant["a"] == 3.96
+    assert substrates[0].bulk.lattice_constant["a"] == 3.92
     assert substrates[0].bulk.main_element == "Pt"
     assert substrates[0].bulk.lattice_type == "fcc"
     assert substrates[0].bulk.ele_dop == "Cu"
@@ -66,8 +66,8 @@ def test_adsorption_part(result):
 #     'StrucInfo': {
 #         'struct': {
 #             'element': 'Pt',
-#             'lattype': 'fcc',
-#             'latcont': 3.92,
+#             'lattice_type': 'fcc',
+#             'lattice_constant': 3.92,
 #             'facet': ['111'],
 #             'dope': {'Cu': [0]}
 #         }
