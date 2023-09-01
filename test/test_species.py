caa96343fce99d679fd1691d37e2993529861b76
zjwang <yczgwangzhaojie@163.com>
2023-09-01 10:28:34 +0800

    20230901_2

--- test/test_species.py
+++ test/test_species.py
@@ -40,7 +40,7 @@ def sml_species():
 def test_get_molecule(sim_species, file_species, sml_species):
     # sim_species
     sim_molecule = sim_species.get_molecule()[0]
-    assert np.allclose(sim_molecule.numbers, [7, 1, 1, 1])
+    assert np.allclose(sim_molecule.get_atomic_numbers(), [7, 1, 1, 1])
     assert np.allclose(
         sim_molecule.positions,
         [
