a047717cce35fb7d3e462932e247e4d0d2eb29d8
zqxian <2821838490@qq.com>
2025-04-30 12:38:36 +0800

    基地中心

--- HTMACat/catkit/gen/adsorption.py
+++ HTMACat/catkit/gen/adsorption.py
@@ -650,7 +650,7 @@ class Builder(AdsorptionSites):
                     max_z = np.max(final_positions[:, 2])  # 获取 slab z 轴最大值
                     base_position[2] = round(0 - min_z + 2.5 + max_z, 1)
 
-                   # 如果输入的 xy 为 [0, 0]，则将分子放在 slab 的 xy 中心
+                   # xzq如果输入的 xy 为 [0, 0]，则将分子放在 slab 的 xy 中心
                     if site_coord[0] == 0 and site_coord[1] == 0:
                        center_x, center_y = utils.center_slab(final_positions)  # 计算 slab 的 xy 中心
                        base_position[0] = round(center_x, 1)  # 将分子放置在 xy 中心
@@ -735,7 +735,7 @@ class Builder(AdsorptionSites):
                # 计算 slab 中心坐标
                 slab_positions = slab.get_positions()
                 center_x, center_y = utils.center_slab(slab_positions)
-               # 如果输入的 xy 为 [0, 0]，则将分子放在 slab 的 xy 中心
+               # xzq如果输入的 xy 为 [0, 0]，则将分子放在 slab 的 xy 中心
                 if site_coord[0] == 0 and site_coord[1] == 0:
                    base_position[0] = round(center_x, 1)
                    base_position[1] = round(center_y, 1)
